687565259470bac0b46decab47e68bb35c22d8aa
forfrontskill <avmakarov.mail@yandex.ru>
2025-02-19 21:44:12 +0300

    Финальная реализация

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -4,13 +4,13 @@
 # Инициализируем бд
 ###
 
-# docker exec -it configSrv mongosh --port 27017 --eval 'rs.initiate({ _id: "config_server", configsvr: true, members: [{ _id: 0, host: "configSrv:27017" }] }); exit();'
+docker exec -it configSrv mongosh --port 27017 --eval 'rs.initiate({ _id: "config_server", configsvr: true, members: [{ _id: 0, host: "configSrv:27017" }] }); exit();'
 
-# docker compose exec -i shard1 mongosh --port 27018 --quiet --eval 'rs.initiate({ _id: "shard1", members: [{ _id: 0, host: "shard1:27018" }] }); exit();'
+docker compose exec -i shard1 mongosh --port 27018 --quiet --eval 'rs.initiate({ _id: "shard1", members: [{ _id: 0, host: "shard1:27018" }] }); exit();'
 
-# docker compose exec -it shard2 mongosh --port 27019 --quiet --eval 'rs.initiate({ _id: "shard2", members: [{ _id: 1, host: "shard2:27019" }] }); exit();'
+docker compose exec -it shard2 mongosh --port 27019 --quiet --eval 'rs.initiate({ _id: "shard2", members: [{ _id: 1, host: "shard2:27019" }] }); exit();'
 
-# docker exec -it mongos_router mongosh --port 27020 --quiet --eval 'sh.addShard("shard1/shard1:27018"); sh.addShard("shard2/shard2:27019"); sh.enableSharding("somedb"); sh.shardCollection("somedb.helloDoc", { "name" : "hashed" }); use somedb; for (var i = 0; i < 1000; i++) db.helloDoc.insert({age: i, name: "ly" + i}); db.helloDoc.countDocuments(); exit();'
+docker exec -it mongos_router mongosh --port 27020 --quiet --eval 'sh.addShard("shard1/shard1:27018"); sh.addShard("shard2/shard2:27019"); sh.enableSharding("somedb"); sh.shardCollection("somedb.helloDoc", { "name" : "hashed" }); use somedb; for (var i = 0; i < 1000; i++) db.helloDoc.insert({age: i, name: "ly" + i}); db.helloDoc.countDocuments(); exit();'
 
 docker exec -it mongos_router \
   mongosh --port 27020 --quiet --eval '
